beb2858f6e0061854d142adfedce355079eba7f4
Prateek Varma <prtk.varma@gmail.com>
2022-07-05 15:36:24 -0600

    created 5 stream components, and hooked them up with React Router in the App comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
 
 const App = () => {
-  return <div>App</div>
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/streams/new" element={<StreamCreate />} />
+          <Route path="/streams/edit" element={<StreamEdit />} />
+          <Route path="/streams/delete" element={<StreamDelete />} />
+          <Route path="/streams/show" element={<StreamShow />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
