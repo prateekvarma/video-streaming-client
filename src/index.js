efad931d0b4089e847e9f866003b44dbd8f82e65
Prateek Varma <prtk.varma@gmail.com>
2022-07-27 19:01:01 -0600

    Redux setup, and a dummy Reducer to make the app work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  replaceMe: () => "Reducer Test Text", //sending dummy reducer just to make Redux work temporarily
+});
