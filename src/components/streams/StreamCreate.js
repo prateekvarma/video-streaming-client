b35389cfcc17ed0d53c393c40d8318cde181608b
Prateek Varma <prtk.varma@gmail.com>
2022-08-23 23:06:49 -0600

    styled redux-form fields and added labels, passed as props

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,14 +4,25 @@ import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
   renderInput(formProps) {
     //if you do a console.log(formProps) - you'll see some functions i.e onChange, value
-    return <input {...formProps.input} />; //the essentially is a short form to do this : onChange={formProps.input.onChange} value={formProps.input.value}
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+        {/* the ""...formProps.input" essentially is a short form to do this : onChange={formProps.input.onChange} value={formProps.input.value} */}
+      </div>
+    );
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        {/*The "label" prop will be passed into the "renderInput" function as a prop */}
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
       </form>
     );
   }
